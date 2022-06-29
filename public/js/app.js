32f0539d3844af6132489d999b4795da84fe6d5d
Pedro Arruda <arruda.pedro15@outlook.com>
2022-06-29 13:27:10 -0300

    atualizando o servidor

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,15 @@
 class TimersDashboard extends React.Component {
   state = {
-    timers: [
-      {
-        title: "Practice squat",
-        project: "Gym Chores",
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: "Bake squash",
-        project: "Kitchen Chores",
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
+    timers: [],
+  };
+
+  componentDidMount() {
+    this.loadTimersFromServer();
+    setInterval(this.loadTimersFromServer, 5000);
+  }
+
+  loadTimersFromServer = () => {
+    client.getTimers((serverTimers) => this.setState({ timers: serverTimers }));
   };
 
   startTimer = (timerId) => {
@@ -31,6 +25,7 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+    client.startTimer({ id: timerId, start: now });
   };
 
   stopTimer = (timerId) => {
@@ -48,12 +43,15 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+    client.stopTimer({ id: timerId, stop: now });
   };
 
   deleteTimer = (timerId) => {
     this.setState({
       timers: this.state.timers.filter((t) => t.id !== timerId),
     });
+
+    client.deleteTimer({ id: timerId });
   };
 
   handleStartClick = (timerId) => {
@@ -81,6 +79,7 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.concat(t),
     });
+    client.createTimer(t);
   };
 
   updateTimer = (attrs) => {
@@ -96,6 +95,8 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.updateTimer(attrs);
   };
 
   render() {
